7921bd8969814b3d693440c87e46c87552560ea2
Griffin Dyer <GDyer8913@slps.org>
2023-05-01 12:45:13 -0500

    Add project files.

---
+++ Wheel of Fortune/Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        '
+        'Button1
+        '
+        Me.Button1.AutoEllipsis = True
+        Me.Button1.Location = New System.Drawing.Point(598, 578)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(189, 53)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ImageList1
+        '
+        Me.ImageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit
+        Me.ImageList1.ImageSize = New System.Drawing.Size(16, 16)
+        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Enabled = False
+        Me.PictureBox1.Location = New System.Drawing.Point(307, 51)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(774, 521)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 30
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1332, 742)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Button1 As Button
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents ImageList1 As ImageList
+End Class
---
+++ Wheel of Fortune/Form1.vb
@@ -0,0 +1,87 @@
+﻿Public Class Form1
+
+
+    Private Async Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim randGen As New Random
+        Dim intRand As Integer
+        Dim delay As ta
+        delay = Task.Delay(1)
+        For IntTimer As Integer = 1 To 50
+            intRand = randGen.Next(1, 26)
+
+            PictureBox1.Image = My.Resources.Picture1
+            Await Task.Delay(50)
+            PictureBox1.Image = My.Resources.Picture2
+            Await Task.Delay(50)
+            PictureBox1.Image = My.Resources.Picture3
+            Await Task.Delay(50)
+            PictureBox1.Image = My.Resources.Picture4
+            Await Task.Delay(50)
+            PictureBox1.Image = My.Resources.Picture5
+            Await Task.Delay(50)
+            PictureBox1.Image = My.Resources.Picture6
+            Await Task.Delay(50)
+            PictureBox1.Image = My.Resources.Picture7
+            Await Task.Delay(50)
+            PictureBox1.Image = My.Resources.Picture8
+            Await Task.Delay(50)
+            PictureBox1.Image = My.Resources.Picture9
+            Await Task.Delay(50)
+            PictureBox1.Image = My.Resources.Picture10
+            Await Task.Delay(50)
+            PictureBox1.Image = My.Resources.Picture11
+            Await Task.Delay(50)
+            PictureBox1.Image = My.Resources.Picture12
+            Await Task.Delay(50)
+            PictureBox1.Image = My.Resources.Picture13
+            Await Task.Delay(50)
+            PictureBox1.Image = My.Resources.Picture14
+            Await Task.Delay(50)
+            PictureBox1.Image = My.Resources.Picture15
+            Await Task.Delay(50)
+            PictureBox1.Image = My.Resources.Picture16
+            Await Task.Delay(50)
+            PictureBox1.Image = My.Resources.Picture17
+            Await Task.Delay(50)
+            PictureBox1.Image = My.Resources.Picture18
+            Await Task.Delay(50)
+            PictureBox1.Image = My.Resources.Picture19
+            Await Task.Delay(50)
+            PictureBox1.Image = My.Resources.Picture20
+            Await Task.Delay(50)
+            PictureBox1.Image = My.Resources.Picture21
+            Await Task.Delay(50)
+            PictureBox1.Image = My.Resources.Picture22
+            Await Task.Delay(50)
+            PictureBox1.Image = My.Resources.Picture23
+            Await Task.Delay(50)
+            PictureBox1.Image = My.Resources.Picture24
+            Await Task.Delay(50)
+            PictureBox1.Image = My.Resources.Picture25
+            Await Task.Delay(50)
+        Next IntTimer
+
+        Select Case
+                Case 1 randGen = 1
+
+
+        End Select
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        'tick += 1
+
+        'If tick = 1 Then
+        '    PictureBox1.Image = My.Resources.Cow
+        'ElseIf tick = 2 Then
+        '    PictureBox1.Image = My.Resources.Screenshot_2023_04_24_133137
+        'ElseIf tick = 4 Then
+        '    PictureBox1.Image = My.Resources.Cow
+        'End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Start()
+    End Sub
+End Class
